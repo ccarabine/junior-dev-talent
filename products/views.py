9cd135c962c564a5fc3ed44d369b6fd615e9436c
ccarabine <ccarabine12@gmail.com>
2022-03-28 15:11:13 +0000

    Add pagination to products

--- products/views.py
+++ products/views.py
@@ -5,16 +5,17 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product, Category
+from django.db.models.functions import Lower
 
 # Internal:
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-from .models import Product
+from util.util import pagination_setup
+from .models import Product, Category
 
 
 def all_products(request):
-    """ 
-    A view to show all products, including sorting and search queries 
+    """
+    A view to show all products, including sorting and search queries
     """
 
     products = Product.objects.all()
@@ -30,13 +31,14 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-            
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -45,13 +47,16 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter any search criteria!")
+                messages.error(
+                    request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-            
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+
+            queries = Q(
+                name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
+    products = pagination_setup(products, request, 6)
 
     context = {
         'products': products,
@@ -61,6 +66,7 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
 
 def product_detail(request, product_id):
     """ A view to show individual product details """
---
+++ util/util.py
@@ -0,0 +1,23 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+# Internal:
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+def pagination_setup(item_list, request, items_per_page):
+    """
+    This function setups pagination, with items per page
+    :return paginated item_list object
+    """
+    paginator = Paginator(item_list, items_per_page)
+    page = request.GET.get('page', 1)
+    try:
+        item_list = paginator.page(page)
+    except PageNotAnInteger:
+        item_list = paginator.page(1)
+    except EmptyPage:
+        item_list = paginator.page(paginator.num_pages)
+    return item_list
