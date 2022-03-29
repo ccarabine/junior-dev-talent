d696e0d8436411d7a9987faccc47d97d9b20a21c
ccarabine <ccarabine12@gmail.com>
2022-03-29 14:19:17 +0000

    Add messages to views in basket and css

--- basket/views.py
+++ basket/views.py
@@ -39,14 +39,16 @@ def add_to_basket(request, item_id):
 def adjust_basket(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
-
     basket = request.session.get('basket', {})
 
     if quantity > 0:
         basket[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {basket[item_id]}')
     else:
         basket.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag')
 
     request.session['basket'] = basket
     return redirect(reverse('view_basket'))
@@ -56,13 +58,14 @@ def remove_from_basket(request, item_id):
     """Remove the item from the shopping basket"""
 
     try:
-
+        product = get_object_or_404(Product, pk=item_id)
         basket = request.session.get('basket', {})
 
         basket.pop(item_id)
-
+        messages.success(request, f'Removed {product.name} from your bag')
         request.session['basket'] = basket
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
--- static/js/base.js
+++ static/js/base.js
@@ -1,16 +1,4 @@
 /*global bootstrap:false*/
 /*jshint esversion:6*/
 
-// Message variables
-const messages = document.getElementById('msg');
-const alert = new bootstrap.Alert(messages);
-
-// vote variables
-
-if (messages)
-  setTimeout(function () {
-    alert.close();
-  }, 5500);
-
-
 $('.toast').toast('show');
