496ffcb28269556b453c01efdce16a99523cef9d
ccarabine <ccarabine12@gmail.com>
2022-03-29 09:49:06 +0000

    Update context processor

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,19 +1,31 @@
 # Imports
 # 3rd party:
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-from django.shortcuts import render
-from django.conf import settings
+from django.shortcuts import get_object_or_404
 
 # Internal:
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+from products.models import Product
 
 def basket_contents(request):
-
+    """
+    Function to add products to the basket
+    """
     basket_items = []
     total = 0
     product_count = 0
+    basket = request.session.get('basket', {})
 
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+        
     grand_total = total
     context = {
         'basket_items': basket_items,
