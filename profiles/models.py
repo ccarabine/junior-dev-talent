6fd38adedf89869eee79f35ddb59486984fdea59
ccarabine <ccarabine12@gmail.com>
2022-03-31 14:57:13 +0000

    Add profile form, views and updated template

--- profiles/models.py
+++ profiles/models.py
@@ -24,21 +24,6 @@ class UserProfile(models.Model):
         null=True,
         blank=True
         )
-    default_country = CountryField(
-        blank_label='Country *',
-        null=True,
-        blank=True
-        )
-    default_postcode = models.CharField(
-        max_length=20,
-        null=True,
-        blank=True
-        )
-    default_town_or_city = models.CharField(
-        max_length=40,
-        null=True,
-        blank=True
-        )
     default_street_address1 = models.CharField(
         max_length=80,
         null=True,
@@ -49,10 +34,25 @@ class UserProfile(models.Model):
         null=True,
         blank=True
         )
+    default_town_or_city = models.CharField(
+        max_length=40,
+        null=True,
+        blank=True
+        )
     default_county = models.CharField(
         max_length=80,
         null=True,
         blank=True)
+    default_postcode = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True
+        )
+    default_country = CountryField(
+        blank_label='Country',
+        null=True,
+        blank=True
+        )
 
     def __str__(self):
         return self.user.username
