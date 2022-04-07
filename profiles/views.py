4492364beeca38c633f427c5098b465ff25b247c
ccarabine <ccarabine12@gmail.com>
2022-04-07 17:41:58 +0000

    Fix media files for profile app

--- profiles/views.py
+++ profiles/views.py
@@ -20,7 +20,7 @@ def edit_profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
-        form = UserProfileForm(request.POST, instance=profile)
+        form = UserProfileForm(request.POST, request.FILES, instance=profile)
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
