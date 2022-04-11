5ef447bbe5515bee80b3df11093a257dd78b34dc
ccarabine <ccarabine12@gmail.com>
2022-04-11 08:26:35 +0000

    Add AWS storage settings and custom_storages

---
+++ custom_storages.py
@@ -0,0 +1,23 @@
+# Imports
+# 3rd party:
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+# Internal:
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class StaticStorage(S3Boto3Storage):
+    """
+    A class for AWS boto static storage
+    """
+    location = settings.STATICFILES_LOCATION
+
+
+class MediaStorage(S3Boto3Storage):
+    """
+    A class for AWS boto media storage
+    """
+    location = settings.MEDIAFILES_LOCATION
