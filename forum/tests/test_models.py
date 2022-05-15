dbf5e2001de745057f743ebb6ff8fa4a50ff6702
ccarabine <ccarabine12@gmail.com>
2022-05-15 13:13:23 +0000

    Add Forum - Test models - Post  model

--- forum/tests/test_models.py
+++ forum/tests/test_models.py
@@ -2,15 +2,19 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 3rd party:
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 # Internal:
-from forum.models import Topic
+from forum.models import Topic, Post
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 class TestTopicModel(TestCase):
     """Test Topic model"""
     def setUp(self):
+        chris_c = User.objects.create_user(
+            username='chris_c', password='p2word')
+        
         self.topic = Topic.objects.create(
             name="Coding",
             friendly_name="Coding",
@@ -18,6 +22,12 @@ class TestTopicModel(TestCase):
             topic_image="topic_img",
 
             )
+        self.post = Post.objects.create(
+            topic=self.topic,
+            title="interview question",
+            owner=chris_c,
+            slug="interview%question"
+        )
 
     def test_topic_model_instance(self):
         """
@@ -33,8 +43,17 @@ class TestTopicModel(TestCase):
 
     def test_topic_model_function_friendly_name(self):
         """ Test Topic model function friendly
-            returns friendly name 
+            returns friendly name
         """
         topic_friendly_name = self.topic.friendly_name
         self.assertEqual(str(topic_friendly_name), "Coding")
-    
+
+    def test_post_model_str_method(self):
+        """ Test Post model string method """
+        post = self.post
+        self.assertEqual((post.__str__()), post.title)
+
+    def test_post_model_get_absolute_url(self):
+        """ Test get_absolute_url """
+        post = self.post
+        self.assertEquals(str(post.get_absolute_url()), '/forum/topic/1')
